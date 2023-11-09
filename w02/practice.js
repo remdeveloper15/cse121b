3b0b65855580d33c0d840c08c8accd09e485ad41
remdeveloper15 <helamanfranco@hotmail.com>
2023-11-08 21:03:22 -0500

    Week 02

---
+++ w02/practice.js
@@ -0,0 +1,21 @@
+var sum = 10 + 20;
+var mul = 10 * 20
+let rest = 10 - 20;
+let name = "Helaman";
+let lastName = "Franco"
+let num1 = 1
+let num2 = 4
+let res = num2 ** 3;
+let res1 = num1 == num2;
+let res2 = num1 <= num2;
+let res3 = num1 != num2;
+let res4 = num1 < 10 && num1 > 0;
+alert(sum);
+alert(mul);
+alert(rest);
+alert(name + lastName);
+alert(res);
+alert(res1);
+alert(res2);
+alert(res3);
+alert(res4);
